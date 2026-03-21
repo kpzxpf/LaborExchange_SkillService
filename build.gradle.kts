bddce05f15b418eb7e0f9234a377cc83c041add0
Nikita Volzhin <vlzh.nik@mail.ru>
2026-03-21 19:39:57 +0500

    add test application.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("io.micrometer:micrometer-registry-prometheus")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
